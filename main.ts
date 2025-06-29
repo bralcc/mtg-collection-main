01a4d1c66ff2e29079a243feb579d467c5ae04d9
bralcc <cedric.bral@vdabcampus.be>
2025-06-29 13:30:21 +0200

    added a simple statusbar linecount function

--- main.ts
+++ main.ts
@@ -3,14 +3,28 @@ import {Plugin} from 'obsidian'
 export default class MTGCollection extends Plugin {
 	statusBarTextElement : HTMLSpanElement;
 
-	onload(): Promise<void> | void {
+	onload() {
 		console.log('Loading plugin.');
 
 		this.statusBarTextElement = this.addStatusBarItem().createEl('span');
 		this.statusBarTextElement.textContent = 'Hello';
+
+		this.app.workspace.on('active-leaf-change', async () => {
+			const file = this.app.workspace.getActiveFile()
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		}
+		})
 	}
 
 	onunload(): void {
 		console.log('Unloading plugin.');
+	}
+
+	private updateLineCount(fileContent?: string) {
+		const count = fileContent ? fileContent.split(/\r\n\|\r|\n/).length : 0;
+		const linesWord = count === 1 ? "line" : "lines";
+		this.statusBarTextElement.textContent = `${count} ${linesWord}`;
 	}
 }
