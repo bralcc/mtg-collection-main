2b121cae52fd87700b86d99db01643727a57196a
bralcc <cedric.bral@vdabcampus.be>
2025-06-30 13:47:19 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,31 @@ export default class MTGCollection extends Plugin {
 	statusBarTextElement : HTMLSpanElement;
 
 	onload() {
-		console.log('Loading plugin.');
-
 		this.statusBarTextElement = this.addStatusBarItem().createEl('span');
-		this.statusBarTextElement.textContent = 'Hello';
+		this.readAndUpdateLineCount();
 
 		this.app.workspace.on('active-leaf-change', async () => {
-			const file = this.app.workspace.getActiveFile()
-		if (file) {
-			const content = await this.app.vault.read(file);
-			this.updateLineCount(content);
-		}
+			this.readAndUpdateLineCount();
 		})
-	}
-
-	onunload(): void {
-		console.log('Unloading plugin.');
+		this.app.workspace.on('editor-change', (editor) => {
+			const content = editor.getDoc().getValue();
+			this.updateLineCount(content);
+		})
 	}
 
 	private updateLineCount(fileContent?: string) {
 		const count = fileContent ? fileContent.split(/\r\n\|\r|\n/).length : 0;
 		const linesWord = count === 1 ? "line" : "lines";
 		this.statusBarTextElement.textContent = `${count} ${linesWord}`;
+	}
+
+	private async readAndUpdateLineCount() {
+		const file = this.app.workspace.getActiveFile()
+		if (file) {
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		} else {
+			this.updateLineCount(undefined);	
+		}
 	}
 }
